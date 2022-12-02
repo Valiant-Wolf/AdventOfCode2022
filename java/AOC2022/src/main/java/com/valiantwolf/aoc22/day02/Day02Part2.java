9e03e5762f26f90854d71fd8b99fc53c24fc275d
Jayme Green <valiant_wolf@hotmail.co.uk>
2022-12-02 18:01:47 +0000

    Refactor Day 2 Java

--- java/AOC2022/src/main/java/com/valiantwolf/aoc22/day02/Day02Part2.java
+++ java/AOC2022/src/main/java/com/valiantwolf/aoc22/day02/Day02Part2.java
@@ -24,19 +24,35 @@ public class Day02Part2
                          Matcher matcher = pattern.matcher( o );
                          //noinspection ResultOfMethodCallIgnored
                          matcher.find();
-                         return Pair.<Shape, Command>of( Shape.parse( matcher.group( 1 ) ), Command.parse( matcher.group( 2 ) ) );
+                         return Pair.of( Shape.parse( matcher.group( 1 ) ), Command.parse( matcher.group( 2 ) ) );
                      } )
-                     .map( o -> o.getLeft().responseFor( o.getRight() ).playedAgainst( o.getLeft() ) )
+                     .map( o -> {
+                         Command command = o.getRight();
+                         Shape response = o.getLeft().responseFor( command );
+                         return command.getScore() + response.getScore();
+                     } )
                      .reduce( 0, Integer::sum )
         );
         System.out.println(i[0]);
     }
 
-    private static enum Command
+    private enum Command
     {
-        WIN,
-        DRAW,
-        LOSE;
+        WIN(6),
+        DRAW(3),
+        LOSE(0);
+
+        private final int score;
+
+        Command(int score)
+        {
+            this.score = score;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
 
         public static Command parse(String string)
         {
@@ -56,23 +72,6 @@ public class Day02Part2
         PAPER(2),
         SCISSORS(3);
 
-        private static final int SCORE_WIN = 6;
-        private static final int SCORE_DRAW = 3;
-        private static final int SCORE_LOSS = 0;
-
-        private static final ImmutableTable<Shape, Shape, Integer> TABLE_SCORE =
-                ImmutableTable.<Shape, Shape, Integer>builder()
-                              .put( ROCK, ROCK, SCORE_DRAW )
-                              .put( ROCK, PAPER, SCORE_WIN )
-                              .put( ROCK, SCISSORS, SCORE_LOSS )
-                              .put( PAPER, ROCK, SCORE_LOSS )
-                              .put( PAPER, PAPER, SCORE_DRAW )
-                              .put( PAPER, SCISSORS, SCORE_WIN )
-                              .put( SCISSORS, ROCK, SCORE_WIN )
-                              .put( SCISSORS, PAPER, SCORE_LOSS )
-                              .put( SCISSORS, SCISSORS, SCORE_DRAW )
-                              .build();
-
         private static final ImmutableTable<Shape, Command, Shape> TABLE_RESPONSE =
                 ImmutableTable.<Shape, Command, Shape>builder()
                               .put( ROCK, Command.LOSE, SCISSORS )
@@ -88,7 +87,7 @@ public class Day02Part2
 
         private final int score;
 
-        private Shape(int score)
+        Shape(int score)
         {
             this.score = score;
         }
@@ -98,10 +97,9 @@ public class Day02Part2
             return TABLE_RESPONSE.get( this, command );
         }
 
-        public int playedAgainst( Shape opponentShape )
+        public int getScore()
         {
-            //noinspection ConstantConditions
-            return score + TABLE_SCORE.get( opponentShape, this );
+            return score;
         }
 
         public static Shape parse( String string )
